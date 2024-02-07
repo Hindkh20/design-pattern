5b418946e6ece6c5c6c35a49378ad877bede15b4
Hindkh20 <hindkhif2002@gmail.com>
2024-02-07 19:06:20 +0100

    done

--- src/main/java/com/hind/org/design/patterns/simpleExample/SingletonClass.java
+++ src/main/java/com/hind/org/design/patterns/simpleExample/SingletonClass.java
@@ -11,21 +11,21 @@ public class SingletonClass {
         // Des initialisations peuvent être ajoutées ici.
     }
 
-    /*
-     * public static SingletonClass getInstance() {
-     * if (instance == null) {
-     * // Creation of instance (only if it does not exist)
-     * instance = new SingletonClass();
-     * }
-     * return instance;
-     * }
-     */
-    // Dans la partie on essai de pour créer une nouvelle instance à chaque appel ce
-    // qui va générer une erreur dans le test
     public static SingletonClass getInstance() {
-        return new SingletonClass();
+        if (instance == null) {
+            // Creation of instance (only if it does not exist)
+            instance = new SingletonClass();
+        }
+        return instance;
     }
 
+    // Dans la partie on essai de pour créer une nouvelle instance à chaque appel ce
+    // qui va générer une erreur dans le test
+    /*
+     * public static SingletonClass getInstance() {
+     * return new SingletonClass();
+     * }
+     */
     public void performTask() {
         System.out.println("task performed !");
     }
