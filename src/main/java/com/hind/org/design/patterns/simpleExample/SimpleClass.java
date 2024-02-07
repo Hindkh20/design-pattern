b9e20f60c548f8d3b1dc3b6f364fb4d5ac3b1cfd
Hindkh20 <hindkhif2002@gmail.com>
2024-02-07 16:57:43 +0100

    commit

---
+++ src/main/java/com/hind/org/design/patterns/simpleExample/SimpleClass.java
@@ -0,0 +1,12 @@
+package com.hind.org.design.patterns.simpleExample;
+
+public class SimpleClass {
+
+    public int add(int firstNumber, int secondNumber) {
+        return 2 * (firstNumber + secondNumber);
+    }
+
+    public int multiply(int firstNumber, int secondNumber) {
+        return firstNumber * secondNumber;
+    }
+}
